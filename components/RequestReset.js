b8849eebb8b77c82a66a5fba29b4509db09be5c5
dnodolf1212 <dnodolf@gmail.com>
2021-05-07 14:03:02 -0700

    create RequestReset component and plug into signin page. Utilize sendUserPasswordResetLink in REQUEST RESET MUTATION

--- components/RequestReset.js
+++ components/RequestReset.js
@@ -5,30 +5,26 @@ import { CURRENT_USER_QUERY } from './User';
 import { useMutation } from '@apollo/client';
 import Error from './ErrorMessage';
 
-const SIGN_UP_MUTATION = gql`
-  mutation SIGN_UP_MUTATION($name: String!, $email: String!, 
-  $password: String!) {
-    createUser(data: {
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION(
+    $email: String!
+    ) {
+    sendUserPasswordResetLink(
       email: $email 
-      name: $name 
-      password: $password
-    }) {
-      id
-      name 
-      email
+      ) {
+      code 
+      message
     }
   }
 `;
 
-export default function SignUp() {
+export default function RequestReset () {
 
   const { inputs, handleChange, resetForm } = useForm({
-    name: '',
-    email: '',
-    password: '',
+    email: ''
   })
 
-  const [signup, { data, loading, error }] = useMutation(SIGN_UP_MUTATION, {
+  const [signup, { data, loading, error }] = useMutation(REQUEST_RESET_MUTATION, {
     variables: inputs, 
     // refetch the currently logged in user
     //refetchQueries: [{ query: CURRENT_USER_QUERY }]
@@ -46,20 +42,12 @@ export default function SignUp() {
 
   return (
   <Form method="POST" onSubmit={handleSubmit}>
-    <h2>Go Ahead and Sign Up for an Account!!</h2>
+    <h2>Request a Password Reset</h2>
 
     <Error error={error} />
     <fieldset>
-  {data?.createUser && <p>Signed up with {data.createUser.email}: Please go Ahead and Sign In!!</p>}
-      <label htmlFor="name">
-        Your Name 
-        <input type="text" name="name" placeholder="Your Name"
-        name="name"
-        autoComplete="name"
-        value={inputs.name} 
-        onChange={handleChange} 
-        />
-      </label>
+  {data?.sendUserPasswordResetLink === null && <p>Success! Check your email for a reset link.</p>}
+      
       <label htmlFor="email">
         Email 
         <input type="email" name="email" placeholder="Your Email Address"
@@ -68,15 +56,9 @@ export default function SignUp() {
         onChange={handleChange} 
         />
       </label>
-      <label htmlFor="password">
-        Password 
-        <input type="password" name="password" placeholder="Your Password"
-        autoComplete="password"
-        value={inputs.password} 
-        onChange={handleChange} 
-        />
-      </label>
-      <button type="submit">Sign In</button>
+      
+      <button type="submit">Request Reset</button>
+
     </fieldset>
   </Form>
   )
