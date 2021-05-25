13920e7b4b97254c7d90ce652831e7a844b1a4e8
dnodolf1212 <dnodolf@gmail.com>
2021-05-25 16:41:54 -0700

    create cartcount component to display number of items in the cart, as it changes. Use css transition and transition group tags to wrap Dot component, add animation to dot.

--- components/Nav.js
+++ components/Nav.js
@@ -3,28 +3,36 @@ import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 import SignOut from './SignOut';
 import { useCart } from '../lib/cartState';
+import CartCount from './CartCount';
 
 export default function Nav() {
   const user = useUser();
-  const { openCart } = useCart(); 
+  const { openCart } = useCart();
   return (
     <NavStyles>
-    <Link href="/products">products</Link>
-    {user && (
+      <Link href="/products">products</Link>
+      {user && (
         <>
-        <Link href="/sell">sell</Link>
-        <Link href="/orders">orders</Link>
-        <Link href="/account">account</Link>
-        <SignOut />
-        <button type="button" onClick={openCart}>My Cart</button>
+          <Link href="/sell">sell</Link>
+          <Link href="/orders">orders</Link>
+          <Link href="/account">account</Link>
+          <SignOut />
+          <button type="button" onClick={openCart}>
+            My Cart
+            <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) => tally + cartItem.quantity,
+                0
+              )}
+            />
+          </button>
         </>
       )}
       {!user && (
         <>
-        <Link href="/signin">Sign In!</Link>
+          <Link href="/signin">Sign In!</Link>
         </>
       )}
-      
-  </NavStyles>
+    </NavStyles>
   );
 }
