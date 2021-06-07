c2b4bd1c513a16b1e4f1e4e3d8e982ff47836958
dnodolf1212 <dnodolf@gmail.com>
2021-06-06 23:15:39 -0700

    create Stripe account and drop key into .env.local. Create Checkout component with Elements comp. from Stripe, as well as CheckoutFormStyles and CardElement. Include prop for submit handler in Form.

--- components/Cart.js
+++ components/Cart.js
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
+import styled from 'styled-components';
 import CartStyles from './styles/CartStyles';
 import { useUser } from './User';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
-import styled from 'styled-components';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { useCart } from '../lib/cartState';
+import { Checkout } from './Checkout';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -15,56 +17,58 @@ const CartItemStyles = styled.li`
   img {
     margin-right: 1rem;
   }
-  h3, p {
+  h3,
+  p {
     margin-right: 0;
   }
 `;
 
-function CartItem({cartItem}){
+function CartItem({ cartItem }) {
   const { product } = cartItem;
-  if(!product) return null;
+  if (!product) return null;
 
   return (
     <CartItemStyles>
-      <img 
-      width="100"
-      src={product.photo.image.publicUrlTransformed} 
-      alt={product.name} />
+      <img
+        width="100"
+        src={product.photo.image.publicUrlTransformed}
+        alt={product.name}
+      />
       <div>
         <h3>{product.name}</h3>
         <p>
           {formatMoney(product.price * cartItem.quantity)}
           --
           <em>
-            {cartItem.quantity} &times; {formatMoney(product.price)}
-             - each
+            {cartItem.quantity} &times; {formatMoney(product.price)}- each
           </em>
         </p>
       </div>
     </CartItemStyles>
-    )
+  );
 }
 
-export default function Cart(){
+export default function Cart() {
   const me = useUser();
   const { cartOpen, closeCart } = useCart();
-  if(!me) return null;
-  
-  return(
+  if (!me) return null;
+
+  return (
     <CartStyles open={cartOpen}>
       <header>
         <Supreme>{me.name}'s Cart</Supreme>
         <CloseButton onClick={closeCart}>&times;</CloseButton>
       </header>
-      
+
       <ul>
-        {me.cart.map(cartItem => <CartItem key={cartItem.id} 
-        cartItem={cartItem} />
-        )}  
+        {me.cart.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
       </ul>
       <footer>
         <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+        <Checkout />
       </footer>
     </CartStyles>
-  ) 
+  );
 }
