8193ab59dc4d2fb034f7f8624e8e0579ef65c543
dnodolf1212 <dnodolf@gmail.com>
2021-06-13 23:33:50 -0700

    create logic to chnage page to view order after submit and to close the cart/ wipe that bought data.

--- components/Checkout.js
+++ components/Checkout.js
@@ -10,7 +10,10 @@ import { useState } from 'react';
 import nProgress from 'nprogress';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import { useRouter } from 'next/dist/client/router';
 import SickButton from './styles/SickButton';
+import { useCart } from '../lib/cartState';
+import { CURRENT_USER_QUERY } from './User';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -39,17 +42,24 @@ const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
 function CheckoutForm() {
   const [error, setError] = useState();
+  // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState();
   const stripe = useStripe();
   const elements = useElements();
+  const router = useRouter();
+  const { closeCart } = useCart();
   const [checkout, { error: graphQLError }] = useMutation(
-    CREATE_ORDER_MUTATION
+    CREATE_ORDER_MUTATION,
+    {
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
   );
+
   async function handleSubmit(e) {
     e.preventDefault();
     setLoading(true);
     // stop form from submitting and turn on loader
-    nProgress.start;
+    nProgress.start();
     // start page transition
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
@@ -72,8 +82,12 @@ function CheckoutForm() {
     console.log('Finished with the order!!');
     console.log(order);
     // change page to view order
+    router.push({
+      pathname: `order/[id]`,
+      query: { id: order.data.checkout.id },
+    });
     // close cart
-
+    closeCart();
     // turn loader off
     setLoading(false);
     nProgress.done();
